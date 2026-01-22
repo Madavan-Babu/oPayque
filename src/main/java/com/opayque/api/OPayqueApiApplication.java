4ded5571134aa7a7c6a26880f05711c6dd3652bf
Maddyology <madavanbabu@gmail.com>
2026-01-22 19:28:30 +0530

    chore: initial project skeleton with opaque security foundation

---
+++ src/main/java/com/opayque/api/OPayqueApiApplication.java
@@ -0,0 +1,13 @@
+package com.opayque.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OPayqueApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OPayqueApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/opayque/api/OPayqueApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.opayque.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OPayqueApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
